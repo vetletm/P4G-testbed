82135d2630d372c6e2c85c18b79888d53518c392
vetletm <vetlemoen@gmail.com>
2021-04-18 14:14:20 +0200

    add build commands to epc bootstrap script

--- scripts/bootstrap_epc.sh
+++ scripts/bootstrap_epc.sh
@@ -23,6 +23,23 @@ git clone --branch fop4_extension_new https://github.com/vetletm/openair-spgwu-t
 git clone --branch vetletm-fix-ansible https://github.com/vetletm/FOP4 "$BASE_DIR/FOP4"
 git clone https://github.com/OPENAIRINTERFACE/openair-epc-fed.git "$BASE_DIR/openair-epc-fed"
 
+cd "$EPC_DIR"
+cd openair-hss
+sudo -E docker build --target oai-hss --tag oai-hss:production --file docker/Dockerfile.ubuntu18.04 .
+cd "$EPC_DIR"
+
+cd openair-mme
+sudo -E docker build --target oai-mme --tag oai-mme:production --file docker/Dockerfile.ubuntu18.04
+cd "$EPC_DIR"
+
+cd openair-spgwc
+sudo -E docker build --target oai-spgwc --tag oai-spgwc:production --file docker/Dockerfile.ubuntu18.04 .
+cd "$EPC_DIR"
+
+cd openair-spgwc
+sudo -E docker build --target oai-spgwc --tag oai-spgwc:production --file docker/Dockerfile.ubuntu18.04 .
+cd "$BASE_DIR"
+
 git clone https://github.com/jafingerhut/p4-guide "$BASE_DIR/p4-guide"
 cd "$BASE_DIR"
 # This script calls exit on finish, nothing can be done after it through vagrant.
