592e890ee190c76a92ecec06c9521d8144f655a6
vetletm <vetlemoen@gmail.com>
2021-08-10 16:17:08 +0200

    remove unnecessary files

--- scripts/epc_start_cassandra.sh
+++
@@ -1,6 +0,0 @@
-sudo docker run --name prod-cassandra -d -e CASSANDRA_CLUSTER_NAME="OAI HSS Cluster" \
-             -e CASSANDRA_ENDPOINT_SNITCH=GossipingPropertyFileSnitch cassandra:2.1
-sudo docker cp openair-hss/src/hss_rel14/db/oai_db.cql prod-cassandra:/home
-sudo docker exec -it prod-cassandra /bin/bash -c "nodetool status"
-Cassandra_IP=`sudo docker inspect --format="{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" prod-cassandra`
-sudo docker exec -it prod-cassandra /bin/bash -c "cqlsh --file /home/oai_db.cql ${Cassandra_IP}"
